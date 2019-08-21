bb97284c35417e790e2daa34ae93be20bda3338d
YarkaDev <42116658+YarkaDev@users.noreply.github.com>
2019-08-21 16:23:40 +0300

    Хорусы

--- src/pocketmine/item/ChorusFruit.php
+++ src/pocketmine/item/ChorusFruit.php
@@ -23,6 +23,11 @@
 
 namespace pocketmine\item;
 
+use pocketmine\math\Vector3;
+use pocketmine\block\Liquid;
+use pocketmine\level\sound\EndermanTeleportSound;
+use pocketmine\level\particle\EnchantParticle;
+use pocketmine\entity\Entity;
 
 class ChorusFruit extends Food {
 	/**
@@ -41,8 +46,57 @@ class ChorusFruit extends Food {
 	public function getFoodRestore() : int{
 		return 4;
 	}
+       
+	public function onConsume(Entity $human) {
+		parent::onConsume($human);
+		$lvl = $human->getLevel();
+		assert($lvl !== null);
+		
+		$minX = $human->getFloorX() - 8;
+		$minY = min($human->getFloorY(), 256) - 8;
+		$minZ = $human->getFloorZ() - 8;
+		
+		$container = new Vector3(0, 0, 0);
+		
+		$maxX = $minX + 16;
+		$maxY = $minY + 16;
+		$maxZ = $minZ + 16;
+		
+		for($attempts = 0; $attempts < 16; ++$attempts){
+			$x = mt_rand($minX, $maxX);
+			$y = mt_rand($minY, $maxY);
+			$z = mt_rand($minZ, $maxZ);
+		
+			while($y >= 0 && !$lvl->getBlock($container->setComponents($x, $y, $z))->isSolid()){
+				$y--;
+			}
 
-	/**
+			if($y < 0){
+				continue;
+			}
+			
+			$blockUp = $lvl->getBlock($container->setComponents($x, $y + 1, $z));
+			$blockUp2 = $lvl->getBlock($container->setComponents($x, $y + 2, $z));
+			
+			if($blockUp->isSolid() || $blockUp instanceof Liquid or $blockUp2->isSolid() or $blockUp2 instanceof Liquid){
+				continue;
+			}
+			
+			$particle = new EnchantParticle($human->asVector3());
+			$lvl->addSound(new EndermanTeleportSound($human->asVector3()));
+			$lvl->addParticle($particle);
+			
+			$human->teleport($container->setComponents($x + 0.5, $y + 1, $z + 0.5));
+			
+			$particle->setComponents($human->x, $human->y, $human->z);
+			$lvl->addSound(new EndermanTeleportSound($human->asVector3()));
+			$lvl->addParticle($particle);
+			
+			break;
+		}
+	}
+
+        /**
 	 * @return float
 	 */
 	public function getSaturationRestore() : float{
