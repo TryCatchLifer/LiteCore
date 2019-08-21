546e06b88d63598c4f227f5d60d1042d4e10de39
SerezhaIvanov <serezha.ivanov02@gmail.com>
2019-08-21 13:05:17 +0200

    Update ShulkerBoxInventory.php

--- src/pocketmine/inventory/ShulkerBoxInventory.php
+++ src/pocketmine/inventory/ShulkerBoxInventory.php
@@ -14,45 +14,41 @@ class ShulkerBoxInventory extends ContainerInventory
 
     protected $holder;
 
-    public function __construct(ShulkerBox $tile)
-    {
+    /**
+     * ShulkerBoxInventory constructor.
+     * @param ShulkerBox $tile
+     */
+    public function __construct(ShulkerBox $tile){
         parent::__construct($tile, InventoryType::get(InventoryType::SHULKER_BOX));
     }
 
-    public function getName(): string
-    {
+    /**
+     * @return string
+     */
+    public function getName(): string{
         return "Shulker Box";
     }
 
-    public function getDefaultSize(): int
-    {
+    /**
+     * @return int
+     */
+    public function getDefaultSize(): int{
         return 27;
     }
 
     /**
-     * Returns the Minecraft PE inventory type used to show the inventory window to clients.
      * @return int
      */
-    public function getNetworkType(): int
-    {
+    public function getNetworkType(): int{
         return InventoryNetworkIds::CONTAINER;
     }
 
-    public function onClose(Player $who)
+    /**
+     * @return ShulkerBox
+     */
+    public function getHolder()
     {
-        if(count($this->getViewers()) === 1){
-            $pk = new BlockEventPacket();
-            $pk->x = $this->getHolder()->getX();
-            $pk->y = $this->getHolder()->getY();
-            $pk->z = $this->getHolder()->getZ();
-            $pk->case1 = 1;
-            $pk->case2 = 0;
-            if(($level = $this->getHolder()->getLevel()) instanceof Level){
-                $level->broadcastLevelSoundEvent($this->getHolder(), LevelSoundEventPacket::SOUND_SHULKERBOX_CLOSED);
-                $level->addChunkPacket($this->getHolder()->getX() >> 4, $this->getHolder()->getZ() >> 4, $pk);
-            }
-        }
-        parent::onClose($who);
+        return $this->holder;
     }
 
     public function onOpen(Player $who)
@@ -72,6 +68,23 @@ class ShulkerBoxInventory extends ContainerInventory
         }
     }
 
+    public function onClose(Player $who){
+        if(count($this->getViewers()) === 1){
+            $pk = new BlockEventPacket();
+            $pk->x = $this->getHolder()->getX();
+            $pk->y = $this->getHolder()->getY();
+            $pk->z = $this->getHolder()->getZ();
+            $pk->case1 = 1;
+            $pk->case2 = 0;
+            if(($level = $this->getHolder()->getLevel()) instanceof Level){
+                $level->broadcastLevelSoundEvent($this->getHolder(), LevelSoundEventPacket::SOUND_SHULKERBOX_CLOSED);
+                $level->addChunkPacket($this->getHolder()->getX() >> 4, $this->getHolder()->getZ() >> 4, $pk);
+            }
+        }
+        $this->getHolder()->saveNBT();
+        parent::onClose($who);
+    }
+
     protected function broadcastBlockEventPacket(bool $isOpen)
     {
         $holder = $this->getHolder();
@@ -82,13 +95,5 @@ class ShulkerBoxInventory extends ContainerInventory
         $pk->eventType = 1;
         $pk->eventData = +$isOpen;
         $holder->getLevel()->addChunkPacket($holder->getX() >> 4, $holder->getZ() >> 4, $pk);
-    }
-
-    /**
-     * @return ShulkerBox
-     */
-    public function getHolder()
-    {
-        return $this->holder;
     }
 }
