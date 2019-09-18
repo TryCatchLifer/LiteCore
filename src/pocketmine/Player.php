16de3ba3418fd3a1bd8f03db13a29de7a45a8d67
genisyspromcpe <khismatov@khismatov.ru>
2019-09-18 09:13:22 +0500

    Fixed some errors

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -1666,6 +1666,9 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 				}
 
 				$this->server->getPluginManager()->callEvent($ev = new InventoryPickupArrowEvent($this->inventory, $entity));
+				if($entity->getBow() !== null and $entity->getBow()->hasEnchantment(Enchantment::INFINITY)) {
+					$ev->setCancelled(true);
+				}
 				if($ev->isCancelled()){
 					continue;
 				}
@@ -1693,6 +1696,7 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 						}
 
 						$this->server->getPluginManager()->callEvent($ev = new InventoryPickupItemEvent($this->inventory, $entity));
+
 						if($ev->isCancelled()){
 							continue;
 						}
